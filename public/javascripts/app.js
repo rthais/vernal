55d9397c324f5bc6cf317457e425c9fa7488fd80
Roberto Thais <roberto.n.thais@gmail.com>
2011-07-27 16:28:47 -0400

    test

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -178,6 +178,7 @@ Vernal.auditMoreButton = function() {
 }
 
 Vernal.init = function(){
+  Vernal.getMore();
 
   $("a.button.new").click(function() {
     Vernal['new']();
@@ -200,6 +201,4 @@ Vernal.init = function(){
 
   // Watch on unload for safety
   $(window).unload(function() { Vernal.watch(true) });
-  
-  setTimeout(Vernal.getMore, 0);
 }
