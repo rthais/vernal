6358938556d0225070f74eddaade18b137166884
Roberto Thais <roberto.n.thais@gmail.com>
2011-07-27 15:06:46 -0400

    typo

--- public/javascripts/expanding-text-area.js
+++ public/javascripts/expanding-text-area.js
@@ -201,7 +201,7 @@ $.fn.expandingTextArea = function() {
             _busyResizing = false;
         }
 
-        _setTimoutHandle = setTimeout(_onInput, 0);
+        _setTimeoutHandle = setTimeout(_onInput, 0);
         $this.unbind('.expandingTextarea')
             .bind('keyup.expandingTextarea', _onInput)
             .bind('keydown.expandingTextarea', _onInput)
