597388a8cc4009ca07878220931f9cf79c8f3326
Roberto Thais <roberto.n.thais@gmail.com>
2011-07-27 16:16:31 -0400

    wait a little longer before attempting to first resize

--- public/javascripts/expanding-text-area.js
+++ public/javascripts/expanding-text-area.js
@@ -201,7 +201,7 @@ $.fn.expandingTextArea = function() {
             _busyResizing = false;
         }
 
-        _setTimeoutHandle = setTimeout(_onInput, 0);
+        _setTimoutHandle = setTimeout(_onInput, 5);
         $this.unbind('.expandingTextarea')
             .bind('keyup.expandingTextarea', _onInput)
             .bind('keydown.expandingTextarea', _onInput)
